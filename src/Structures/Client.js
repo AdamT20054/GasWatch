fd2c9772d5387e84474a998ba536ac9f82ac675a
AdamT20054 <Git@AdamT05.com>
2023-08-13 14:19:34 +0100

    Updated gas API. Currently untested.

--- src/Structures/Client.js
+++ src/Structures/Client.js
@@ -1,6 +1,6 @@
 /** @format */
 // @ts-check
-const { default: axios } = require('axios');
+const {default: axios} = require('axios');
 const Discord = require("discord.js");
 const Command = require("./Command.js");
 const Event = require("./Event.js");
@@ -10,116 +10,120 @@ const config = require("../Data/config.json");
 const cooldown = new Set();
 
 function cooldownfunc() {
-	var cooldownvalue = fs.readFileSync("cooldownvalue.json").toString();
-	cooldown.add("1");
-	setTimeout(() => {
-		cooldown.delete("1");
-	// @ts-ignore
-	}, cooldownvalue);
-};
+    var cooldownvalue = fs.readFileSync("cooldownvalue.json").toString();
+    cooldown.add("1");
+    setTimeout(() => {
+        cooldown.delete("1");
+        // @ts-ignore
+    }, cooldownvalue);
+}
 
 
 function ping() {
-	if (cooldown.has("1")) {
-		return false
-	}
-	else {
-		return true
-	}
-};
+    if (cooldown.has("1")) {
+        return false
+    } else {
+        return true
+    }
+}
 
 class Client extends Discord.Client {
-	constructor() {
-		super({ intents, allowedMentions: { repliedUser: false} });
+    constructor() {
+        super({intents, allowedMentions: {repliedUser: false}});
 
-		/**
-		 * @type {Discord.Collection<string, Command>}
-		 */
-		this.commands = new Discord.Collection();
+        /**
+         * @type {Discord.Collection<string, Command>}
+         */
+        this.commands = new Discord.Collection();
 
-		this.prefix = config.prefix;
+        this.prefix = config.prefix;
 
-		this.once('ready', () => { this.watchGas(); });
-	}
+        this.once('ready', () => {
+            this.watchGas();
+        });
+    }
 
+    get endpoint() {
+        return `https://data.spiceai.io/eth/v0.1/gasfees`;
+    }
 
-	
-	async watchGas() {
-		const res = await axios.get(this.endpoint).catch(console.error);
-		const gasvalue = fs.readFileSync("gasvalue.json").toString();
+    async watchGas() {
+        const res = await axios.get(this.endpoint).catch(console.error);
+        const gasvalue = fs.readFileSync("gasvalue.json").toString();
         console.log(res)
 
         if (!res)
-            return setTimeout(() => { this.watchGas(); }, 500000);
+            return setTimeout(() => {
+                this.watchGas();
+            }, 500000);
 
-		const gas = res.data.average;
-		
-		if ((gas) <= gasvalue && (ping() == true )) {
-			cooldownfunc()
-			const slow = res.data.safelow;
-            const standard = res.data.average;
-            const fastest = res.data.fastest;      
+        const gas = res.data.normal;
 
-			// Construct embed
-			const pingEmbed = new Discord.MessageEmbed();
+        if ((gas) <= gasvalue && (ping() == true)) {
+            cooldownfunc()
+            const slow = res.data.slow;
+            const standard = res.data.normal;
+            const fastest = res.data.instant;
+
+            // Construct embed
+            const pingEmbed = new Discord.MessageEmbed();
             pingEmbed.setTitle(`Ethereum has low gas!`)
                 .setDescription(`<@&${config.RoleID}> ETH gas value is less than **${gasvalue}** *gwei*!`)
                 .setColor("#00FFFF")
-				.addFields({
-					name: "Fastest",
-					value: `${fastest}`,
-					inline: true
-				},{
-					name: "Standard",
-					value: `${standard}`,
-					inline: true
-				}, {
-					name: "Slowest",
-					value: `${slow}`,
-					inline: true
-				});
+                .addFields({
+                    name: "Fastest",
+                    value: `${fastest}`,
+                    inline: true
+                }, {
+                    name: "Standard",
+                    value: `${standard}`,
+                    inline: true
+                }, {
+                    name: "Slowest",
+                    value: `${slow}`,
+                    inline: true
+                });
 
 
-			this.channels.fetch(`${config.ChannelID}`)
-				// @ts-ignore
-				.then(channel =>channel.send({ embeds: [pingEmbed] }))
-  				.catch(console.error);
-		};
+            this.channels.fetch(`${config.ChannelID}`)
+                // @ts-ignore
+                .then(channel => channel.send({embeds: [pingEmbed]}))
+                .catch(console.error);
+        }
 
-		setTimeout(() => { this.watchGas(); }, 300000);  // 3600000 = 1hr 900000 = 15min 300000 = 5min
-	}
 
-	get endpoint() {
-		return `https://www.ethgasstation.info/api/ethgasAPI.json`;
-	}
+        setTimeout(() => {
+            this.watchGas();
+        }, 300000);  // 3600000 = 1hr 900000 = 15min 300000 = 5min
+    }
 
-	start(token) {
-		console.log(`Bot is starting up...`);
+    start(token) {
+        console.log(`Bot is starting up...`);
 
-		fs.readdirSync("./src/Commands")
-			.filter(file => file.endsWith(".js"))
-			.forEach(file => {
-				/**
-				 * @type {Command}
-				 */
-				const command = require(`../Commands/${file}`);
-				console.log(`Command ${command.name} loaded`);
-				this.commands.set(command.name, command);
-			});
+        fs.readdirSync("./src/Commands")
+            .filter(file => file.endsWith(".js"))
+            .forEach(file => {
+                /**
+                 * @type {Command}
+                 */
+                const command = require(`../Commands/${file}`);
+                console.log(`Command ${command.name} loaded`);
+                this.commands.set(command.name, command);
+            });
 
-		fs.readdirSync("./src/Events")
-			.filter(file => file.endsWith(".js"))
-			.forEach(file => {
-				/**
-				 * @type {Event}
-				 */
-				const event = require(`../Events/${file}`);
-				console.log(`Event ${event.event} loaded`);
-				this.on(event.event, event.run.bind(null, this));
-			});
+        fs.readdirSync("./src/Events")
+            .filter(file => file.endsWith(".js"))
+            .forEach(file => {
+                /**
+                 * @type {Event}
+                 */
+                const event = require(`../Events/${file}`);
+                console.log(`Event ${event.event} loaded`);
+                this.on(event.event, event.run.bind(null, this));
+            });
 
-		this.login(token);
-	}
+        this.login(token);
+    }
 }
 
 module.exports = Client;
